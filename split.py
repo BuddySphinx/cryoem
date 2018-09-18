c2f769ee86a18ceb790b40d511599801b5f45e94
BuddySphinx <yangqi.gu@yale.edu>
2018-09-18 00:05:37 -0400

    basic split without fixing the coordinates

--- split.py
+++ split.py
@@ -4,11 +4,10 @@ Created on Sun Sep 16 22:45:49 2018
 
 @author: yangq
 """
-#This script is to split particles and save particles for each micrograph
+#This script is to split particles for each micrograph
 import sys
 import os
 input_file=sys.argv[1]
-count=0
 header=[]
 temp=[]
 with open(input_file,'r') as input_star:
@@ -18,25 +17,30 @@ with open(input_file,'r') as input_star:
             line=line.split(" ")
             if line[0].startswith("_rln") or line[0].startswith("data_") or line[0].startswith("loop_") or line[0]=='\n':
                 #Store the variable DefocusV and DefocusV
-                if line[0]=="_rlnMicographName": 
+                if line[0]=="_rlnMicrographName": 
                    newline=line[1].split("#")
                    #global DefocusVindex
                    MicrographNameindex=int(newline[1])
-                if line[0]=="_rln
-                count+=1
+                line=' '.join(line)
                 header.append(line)
+            
             else:
-                temp.append(line)
-                
-
-
-#with open('test.txt','w') as output_star:
-        #for line in temp:
-            #line=' '.join(line)
-            #output_star.write(line)
-        
-        #head=head[1].split("#")
-        #headernumber=int(head[1])
-        #temp=
-                
-    
+                line=' '.join(line)
+                newline=line.split()
+                temp.append(newline)
+    #temp=temp[0:-2] #chop off the last space
+    #Sort list according to the micrograph
+    temp.sort(key=lambda x:x[MicrographNameindex-1])
+    MicrographName=""
+for i in range(0,len(temp)):
+    basename=temp[i][MicrographNameindex-1].split("/")[-1].split(".")[0]
+    with open("{}_split.txt".format(basename),'a') as output_star:
+        #split the file according to their MicrographName
+        if temp[i][MicrographNameindex-1]!=MicrographName:
+            output_star.writelines(header)
+            line=' '.join(temp[i])
+            output_star.write(' ' + line + '\n')
+        else:
+            line=' '.join(temp[i])
+            output_star.write(' ' + line + '\n')
+    MicrographName=temp[i][MicrographNameindex-1]
