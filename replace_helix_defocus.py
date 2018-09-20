62f8bb2405234fee07026ae91b109c5f5399d87f
BuddySphinx <yangqi.gu@yale.edu>
2018-09-19 22:24:59 -0400

    fix the bugs when coordinates have no particles extracted

--- replace_helix_defocus.py
+++ replace_helix_defocus.py
@@ -51,13 +51,13 @@ with open("{}_replaced.star".format(root),'w') as output_star:
 if particles[-1]=='\n':
     particles=particles[0:-2]
 #Split particle files according to their micrographname
-micrograph_particles={name.split("/")[-1].split(".")[0]:list(micrograph_name) for name,micrograph_name in groupby(particles,lambda x:x[MicrographNameindex-1])}
+micrograph_particles={name.split("/")[-1].split(".")[0]:list(micrograph_name) for name,micrograph_name in groupby(particles,lambda x:x[MicrographNameindex])}
 #Replace U,V,A according to the local file
 for local_star in local_stars:
     micrograph=local_star.split(suffix)[0]
     try:
         micrograph_particle=micrograph_particles[micrograph]
-        helical_particles={ID:list(helix) for ID, helix in groupby(micrograph_particle,lambda x:x[helicalIDindex-1])}
+        helical_particles={ID:list(helix) for ID, helix in groupby(micrograph_particle,lambda x:x[helicalIDindex])}
         #Find the keys of the helix dictionary
         IDs=[ID for ID in helical_particles]
         IDs_sorted=sorted(IDs)
@@ -80,24 +80,27 @@ for local_star in local_stars:
                     local_p_new=' '.join(local_p)
                     local_p_new=local_p_new.split()
                     local_ps.append(local_p_new)
+        try:
         #Record helix ID number
-        new_micrograph_particles=[]
-        for ID in IDs_sorted:
-            local_ID=2*int(ID)-2
-            helix_ID=int(ID)-1
-            helix_length=int(helical_len[helix_ID])
+            new_micrograph_particles=[]
+            for ID in IDs_sorted:
+                local_ID=2*int(ID)-2
+                helix_ID=int(ID)-1
+                helix_length=int(helical_len[helix_ID])
             #Record the linear difference according to the defocus A,U,Vstart and end of the helix
-            diffU=(float(local_ps[local_ID+1][local_DefocusUindex])-float(local_ps[local_ID][local_DefocusUindex]))/helix_length
-            diffV=(float(local_ps[local_ID+1][local_DefocusVindex])-float(local_ps[local_ID][local_DefocusVindex]))/helix_length
-            diffA=(float(local_ps[local_ID+1][local_DefocusAindex])-float(local_ps[local_ID][local_DefocusAindex]))/helix_length
-            for ii in range(0,helix_length):
+                diffU=(float(local_ps[local_ID+1][local_DefocusUindex])-float(local_ps[local_ID][local_DefocusUindex]))/helix_length
+                diffV=(float(local_ps[local_ID+1][local_DefocusVindex])-float(local_ps[local_ID][local_DefocusVindex]))/helix_length
+                diffA=(float(local_ps[local_ID+1][local_DefocusAindex])-float(local_ps[local_ID][local_DefocusAindex]))/helix_length
+                for ii in range(0,helix_length):
                 #Update the defocus U,V,A for the helical segments
-                helical_particles[ID][ii][DefocusUindex]=local_ps[local_ID][local_DefocusUindex]+diffU*ii
-                helical_particles[ID][ii][DefocusVindex]=local_ps[local_ID][local_DefocusVindex]+diffV*ii
-                helical_particles[ID][ii][DefocusAindex]=local_ps[local_ID][local_DefocusAindex]+diffA*ii
-            new_micrograph_particles=new_micrograph_particles.extend(helical_particles[ID])
-        #update the micrograph particles
-        micrograph_particles[micrograph]=new_micrograph_particles
+                    helical_particles[ID][ii][DefocusUindex]=str(float(local_ps[local_ID][local_DefocusUindex])+diffU*ii)
+                    helical_particles[ID][ii][DefocusVindex]=str(float(local_ps[local_ID][local_DefocusVindex])+diffV*ii)
+                    helical_particles[ID][ii][DefocusAindex]=str(float(local_ps[local_ID][local_DefocusAindex])+diffA*ii)
+                new_micrograph_particles.extend(helical_particles[ID])
+                #update the micrograph particles
+            micrograph_particles[micrograph]=new_micrograph_particles
+        except IndexError:
+            pass
     except KeyError:
         print("{}.mrc does not exist in your particles".format(micrograph))
         print("make sure your micrographs are consistent in local defocus star file and particle file")
